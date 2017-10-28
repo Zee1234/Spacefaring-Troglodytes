9f36444c78d1ee9699c1c4c0056c1e3631b17c87
Zee1234 <zeeatgit@gmail.com>
2017-10-27 23:50:58 -0500

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,67 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+let rules = {}
+rules.stylus = {
+  test: /\.styl$/,
+  use: [
+    {
+      loader: 'css-loader'
+    },
+    {
+      loader: 'stylus-loader',
+      options: {
+        use: [require('nib')()],
+        import: ['~nib/lib/nib/index.styl']
+      },
+    },
+  ],
+}
+
+rules.pug = {
+  test: /\.pug$/,
+  use: [
+    {
+      loader: 'pug-loader',
+      options: {
+        pretty: true,
+        data: {
+          hi: 'Hello World!',
+          require,
+          __dirname
+        }
+      }
+    }
+  ]
+}
+
+rules.js = {
+  test: /\.(js|mjs)$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        ['env', {
+          "targets": {
+            "browsers": ["last 2 versions", "safari >= 7"]
+          }
+        }],
+      ]
+    }
+  }
+}
+
+rules.images = {
+  test: /\.(png|jpg|gif)$/,
+  use: [
+    {
+      loader: 'file-loader',
+      options: {}
+    }
+  ]
+}
+
 
 
 
@@ -8,7 +69,6 @@ module.exports = {
   stats: {
     colors: false, // Babel screws up the look of my command line. Known bug, not fixed. So work around it.
   },
-
 
   entry: {
     game: path.resolve(__dirname, 'src','scripts','main.js')
@@ -21,62 +81,10 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.styl$/,
-        use: [
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'stylus-loader',
-            options: {
-              use: [require('nib')()],
-              import: ['~nib/lib/nib/index.styl']
-            },
-          },
-        ],
-      },
-      {
-        test: /\.pug$/,
-        use: [
-          {
-            loader: 'pug-loader',
-            options: {
-              pretty: true,
-              data: {
-                hi: 'Hello World!',
-                require,
-                __dirname
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(js|mjs)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['env', {
-                "targets": {
-                  "browsers": ["last 2 versions", "safari >= 7"]
-                }
-              }],
-            ]
-          }
-        }
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      }
+      rules.stylus,
+      rules.pug,
+      rules.js,
+      rules.images
     ],
   },
 
